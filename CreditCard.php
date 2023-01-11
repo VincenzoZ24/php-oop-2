9a17a1c6c86aa3952c1a442fd195ef0cec41c098
VincenzoZ24 <zitov287@gmail.com>
2023-01-11 16:10:32 +0100

    add

---
+++ CreditCard.php
@@ -0,0 +1,45 @@
+<?php
+
+class CreditCard {
+	private string $id;
+	private string $number;
+	private int $expiryYear;
+	private int $expiryMonth;
+
+	public function __construct(string $number, int $expiryYear, int $expiryMonth)
+	{
+		
+		$this->number = $number;
+		$this->expiryYear = $expiryYear;
+		$this->expiryMonth = $expiryMonth;
+	}
+
+	public function isExpired(): bool {
+		if ((int) date('y') < $this->expiryYear) {
+			return false;
+		} else if ((int) date('y') == $this->expiryYear && (int) date('n') < $this->expiryMonth) {
+			return false;
+		}
+		return true;
+	}
+
+	public function save() {
+		
+		$this->id = 'id from database';
+		return $this;
+	}
+
+	public static function get(int $id) {
+		
+	}
+
+	public function pay(float $amount): bool {
+		if ($this->isExpired()) {
+			
+			return false;
+		} else {
+			
+			return true;
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
 
-    include_once __DIR__ . '/UtenteRegistrato.php';
-    include_once __DIR__ . '/ECommerce.php';
-    include_once __DIR__ . '/Card.php';
-    include_once __DIR__ . '/UtenteRegistrato.php';
+    
+    include_once __DIR__ . '/CreditCard.php';
+    include_once __DIR__ . '/User.php';
+    include_once __DIR__ . '/GuestUser.php';
+    include_once __DIR__ . '/RegisteredUser.php';
     include_once __DIR__ . '/Cibo.php';
     include_once __DIR__ . '/Cucce.php';
     include_once __DIR__ . '/Giochi.php';
-    
+
+
     ?>
 </body>
+
 </html>
